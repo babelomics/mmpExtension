5cca5966bfbc7986d37bbddec2214a4c00327d7c
Alexis Martínez Chacón <alexismartinezchacon@gmail.com>
2018-02-22 10:01:33 +0100

    Set up host swagger by config

--- config/main.js
+++ config/main.js
@@ -3,6 +3,8 @@ module.exports = {
   secret: 'My_Secret_KEY',
   // Database connection information
   database: 'mongodb://mongo:27017/MMP-Extension',
+  // host
+  host: process.env.HOST || 'localhost',
   // Setting port for server
   port: process.env.PORT || 3000,
 };
--- config/swagger.js
+++ config/swagger.js
@@ -1,10 +1,13 @@
 module.exports.swagger_init = function(app, express) {
   const YAML = require('yamljs');
+  const config = require('./main');
   const swaggerUi = require('swagger-ui-express');
   const swaggerModelValidator = require('swagger-model-validator');
 
   const routerSwagger = express.Router();
   const options = YAML.load('./data/swagger/doc.yaml');
+  options.host = config.host + ':' + config.port;
+
   routerSwagger.use('/', swaggerUi.serve, swaggerUi.setup(options));
 
   // Set auth routes as subgroup/middleware to /api/docs
